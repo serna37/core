a04f04ef054422776821305473958f17e31be244
serna <37serna37serna37serna@gmail.com>
2022-02-05 16:13:37 +0900

    ajax更新

--- Ajax.js
+++ Ajax.js
@@ -1,54 +1,26 @@
-/*
-==================================================
-Ajaxの共通基底処理
-==================================================
-*/
-export default class Ajax {
-  constructor(){};
+// ========================================
+// Ajaxの共通基底処理
+// ========================================
+const Ajax = {
 
-  // TODO GET送信
-  get = () => {console.log("GET送信")};
-  // TODO POST送信
-  post = (req, url, func, efunc) => {
-    fetch(url, {
-      // FIXME ここはoiptionとして外だししたい
-      method: "POST",
-      body: JSON.stringify(req)
-    })
-    .then(data => func(JSON.parse(data)))
-    .catch(e => efunc(e));
-  };
-
-  // TODO あとで消す
-  test = v => (...args) => args
-    .reduce((x, y) => x.concat(y))
-    .forEach((e, i) => console.log(`index: ${i}, value: ${e}`));
+  post(request, func, efunc) {
+    const url = 'https://neras-sta.com/cgi-bin/api.py';
+    const method = "POST";
+    const headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    };
+    const body = JSON.stringify(request);
+    // exec
+    fetch(url, {method, headers, body})
+      .then(res => res.json())
+      .then(v => func(v))
+      .catch(e => {
+        console.error(e);
+        if (efunc) {
+          efunc(e);
+        }
+      });
+  },
 }
-
-
-
-// let json = {
-//   id: 'test',
-//   name: 'aa',
-//   do: 'aa'
-// };
-// var url = "https://neras-sta.com/cgi-bin/text.py";
-// var method = "POST";
-// var headers = {
-//   'Accept': 'application/json',
-//   'Content-Type': 'application/json',
-// };
-// var body = JSON.stringify(json);
-// var mhb = {method, headers, body}
-// fetch(url, mhb)
-// .then(v => {
-//   console.log(v)
-//   return v
-// })
-// .then(res => res.json())
-// .then(v => {
-//   console.log(v)
-//   return v
-// })
-// .then(v => console.log(v))
-// .catch(e => console.log(e));
+export default Ajax;
